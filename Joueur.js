1c04bf299e5ae1fd299882a525f1a2f4a591d5ce
Unknown <willisdelamour@gmail.com>
2018-03-09 12:53:07 +0100

    Création Joueur&Plateau

---
+++ Joueur.js
@@ -0,0 +1,26 @@
+class Joueur{
+var pseudo
+var argent
+var position
+var prison
+
+constructor(pseudo, argent){
+this.pseudo = pseudo;
+this.argent=argent;
+position = 0;
+prison = 0;
+}
+
+function lancerDe(){
+let de1 = Math.floor(Math.random()*6)+1;
+let de2 = Math.floor(Math.random()*6)+1;
+
+if(de1 == de2){
+de1+=lancerDe();
+}
+
+return de1+de2;
+}
+
+
+}
