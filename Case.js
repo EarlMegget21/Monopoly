d127f7f8ce47a4a01fd1def223e2f8dff545bb51
Unknown <willisdelamour@gmail.com>
2018-03-09 13:38:59 +0100

    getters/setter

--- Case.js
+++ Case.js
@@ -1,8 +1,26 @@
 class Case{
-
+	var nom
     var node
 
-    constructor(node){
+    constructor(node,nom,couleur){
         this.node = node
+        this.nom = nom
+        this.couleur = couleur
+    }
+        
+    function getNode(){
+    return this.node;
+    }
+    
+    function setNode(node){
+    this.node = node;
+    }
+    
+    function getNom(){
+    return nom;
+    }
+    
+    function getCouleur(){
+    return couleur;
     }
 }
--- Terrain.js
+++ Terrain.js
@@ -1,10 +1,15 @@
 class Terrain extends Case{
 
-    constructor(node, prixAchat, loyer, couleur){
+    constructor(node, prixAchat, loyer, couleur, nom, couleur){
         this.node = node
         this.prixAchat = prixAchat
         this.loyer = loyer
         this.couleur = couleur
         this.propriétaire = null
+        this.nom = nom
+        this.couleur = couleur
     }
+
+    
+    
 }
