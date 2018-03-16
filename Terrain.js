8127d7b1d8cbc83e2ab4b8990360467f12eb638c
Julien <papajr@outlook.fr>
2018-03-16 11:27:31 +0100

    MàJ Prison

--- Terrain.js
+++ Terrain.js
@@ -24,11 +24,18 @@ class Terrain extends Case{
         if(this.proprietaire == null){
             this.acheterTerrain(joueur)
         }else{
-            console.log("Le joueur " + joueur.couleur + " doit " + this.loyer + "€ au joueur " +  this.proprietaire.couleur) //TODO afficher à l'écran avec un délai
-            joueur.retirerSous(this.loyer)
-            this.proprietaire.ajouterSous(this.loyer)
+            if(this.proprietaire === joueur) {
+                console.log("Vous êtes chez vous")
+            }else if (this.proprietaire.prison != 0) {
+                console.log("Le joueur " + this.proprietaire.couleur + " est en prison, vous ne devez rien")
+            } else {
+                console.log("Le joueur " + joueur.couleur + " doit " + this.loyer + "€ au joueur " + this.proprietaire.couleur) //TODO afficher à l'écran avec un délai
+                joueur.retirerSous(this.loyer)
+                this.proprietaire.ajouterSous(this.loyer)
+            }
         }
     }
+
 
     acheterTerrain(joueur){
         console.log("Le terrain coûte " + this.valeur + "€")
